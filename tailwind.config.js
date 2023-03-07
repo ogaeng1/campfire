47c7ed7a7b08e58be75a2f195046ffea12afa828
OH <rudals2148@naver.com>
2023-03-07 23:56:51 +0900

    애니메이션 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spinner: "spin 1.5s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
